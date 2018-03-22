06e54f99a9e30ae82586ef5466da446ebb61e332
roccomuso <roccomusolino92@gmail.com>
2018-03-22 13:06:45 +0100

    goodUri a real persistent db

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ import keyvTestSuite, { keyvOfficialTests } from '@keyv/test-suite';
 import Keyv from 'keyv';
 import KeyvLevelDB from 'this';
 
-keyvOfficialTests(test, Keyv, 'leveldb://:memory:', 'leveldb://:null:');
+keyvOfficialTests(test, Keyv, 'leveldb://DB_TEST', 'leveldb://:null:');
 
 const store = () => new KeyvLevelDB();
 keyvTestSuite(test, Keyv, store);
